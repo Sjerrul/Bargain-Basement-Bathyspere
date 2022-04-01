b5deca1f11778171f64dd70d7d1fd841b69d6b8d
Céryl Wiltink <ceryl.wiltink@trifork.nl>
2022-04-01 14:34:36 +0200

    Initial board layout

---
+++ Assets/Scripts/Token.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+    public Square CurrentSquare;
+    public int PowerModifier;
+    public int OxygenModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnMouseUp()
+    {
+        Debug.Log("Click Token");
+        this.CurrentSquare = this.CurrentSquare.NextSquare;
+        this.transform.position = this.CurrentSquare.transform.position;
+    }
+}
